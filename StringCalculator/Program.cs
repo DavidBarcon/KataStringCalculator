9fcce952e29bfc0f6f7dbb1eb116b4f3a7853b4f
David Barcón <dbarcon@domingoalonsogroup.com>
2023-10-05 11:21:22 +0100

    Implemented step 3 functionality added some comments

--- StringCalculator/Program.cs
+++ StringCalculator/Program.cs
@@ -14,7 +14,12 @@
 
         public int add(String numbers)
         {
-            string[] values = numbers.Split(',');
+            //copy string and replace \n by ,
+            string nums = (string)numbers.Clone();
+            nums = nums.Replace("\n", ",");
+
+            //divide the sring and sum its elements
+            string[] values = nums.Split(',');
             int sum = 0;
             foreach (string value in values)
             {
