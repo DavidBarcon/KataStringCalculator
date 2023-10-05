21cf54bbb844916c061782f048d346ff2225358c
David Barcón <dbarcon@domingoalonsogroup.com>
2023-10-05 11:41:37 +0100

    Implemented step 4 functionality

--- StringCalculator/Program.cs
+++ StringCalculator/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace StringCalculator
+﻿using System.Globalization;
+
+namespace StringCalculator
 {
     internal class Program
     {
@@ -14,12 +16,22 @@
 
         public int add(String numbers)
         {
-            //copy string and replace \n by ,
+            char separator = ',';
+            //copy the string to modify it
             string nums = (string)numbers.Clone();
-            nums = nums.Replace("\n", ",");
+
+            //get the separator if specified
+            if (numbers.Contains("//"))
+            {
+                separator = numbers[2];
+                nums = nums.Substring(3);
+            }
+
+            //replace \n by the separator
+            nums = nums.Replace("\n", separator.ToString());
 
             //divide the sring and sum its elements
-            string[] values = nums.Split(',');
+            string[] values = nums.Split(separator);
             int sum = 0;
             foreach (string value in values)
             {
